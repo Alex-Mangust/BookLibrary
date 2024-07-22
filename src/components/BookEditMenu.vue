64e150de5bccf1ab7a6f4fbd0289b373fa5656d5
Alex Mangust <sanya.stogny@gmail.com>
2024-07-23 00:26:11 +0800

    Исправил проблему из-за которой, при редактировании, книга дублировалась. Настроил стили для меню редактирования

--- src/components/BookEditMenu.vue
+++ src/components/BookEditMenu.vue
@@ -2,41 +2,63 @@
 export default {
     name: "BookEditMenu",
     props: ["book"],
+    data() {
+        return {
+            colorOpenButton: "",
+            backgroundOpenButton: ""
+        }
+    },
     methods: {
         save() {
             this.$el.style.display = "none";
             this.$emit("show");
             this.$emit("update");
         },
+        async downoloadImage() {
+            const filePaths = await window.api.openFileDialog();
+            this.book.images = filePaths[0];
+        }
+    },
+    mounted() {
+        switch (this.book.status) {
+            case "reading":
+                this.backgroundOpenButton = "rgb(127, 255, 212)";
+                this.colorOpenButton = "#008500";
+                break;
+            case "wanttoread":
+                this.backgroundOpenButton = "rgb(139, 0, 0)";
+                this.colorOpenButton = "#FF4040	";
+                break;
+            case "finishread":
+                this.backgroundOpenButton = "rgb(222, 184, 135)";
+                this.colorOpenButton = "#BF7A30";
+                break;
+            default:
+                this.backgroundOpenButton = "white";
+                this.colorOpenButton = "black";
+                break;
+        }
     }
 }
 </script>
 
 <template>
     <div class="inside_edit_book_menu">
-        <div class="inputs">
-            <div class="form-group">
-                <label>Введите название книги:</label>
-                <input v-model="this.book.title" type="text" required>
-            </div>
-            <div class="form-group">
-                <label>Введите описание книги:</label>
-                <input v-model="this.book.description" type="text">
-            </div>
-            <div class="form-group">
-                <label>Вставьте ссылку на источник книги:</label>
-                <input v-model="this.book.link" type="text">
-            </div>
-            <div class="form-group">
-                <label>Вставьте ссылку на обложку книги:</label>
-                <div>
-                    <input v-model="this.book.images" type="text">
-                    <button @click="downoloadImage">Или загрузите изображение</button>
-                </div>
+        <input :style="{ color: colorOpenButton, fontSize: `2em`, textAlign: `center` }" v-model="this.book.title"
+            type="text" required>
+        <div class="edit_img">
+            <img :src="this.book.images" :alt="this.book.alt">
+            <div>
+                <input v-model="this.book.images" type="text">
+                <button @click="downoloadImage">Или загрузите изображение</button>
             </div>
         </div>
+        <div class="description_data_book">
+            <textarea v-model="this.book.description" :style="{ width: '70%', height: '90%', color: colorOpenButton }"
+                placeholder="Введите описание книги...">
+            </textarea>
+        </div>
         <select class="select_status_book" v-model="this.book.status">
-            <option value="none">Выберите статус</option>
             <option value="reading">Читаю</option>
             <option value="wanttoread">Хочу прочитать</option>
             <option value="finishread">Закончил читать</option>
--- src/components/DataBook.vue
+++ src/components/DataBook.vue
@@ -22,11 +22,12 @@ export default {
             this.$emit("displayOff");
         },
         changeBookStatus() {
-            this.$emit("delete");
             this.book.status = this.bookStatus;
             this.updateBook();
         },
         updateBook() {
+            this.$emit("delete");
+            this.book.alt = `Иконка книги ${this.book.title}`;
             this.$emit("update", this.book);
         },
         openLink() {
@@ -40,7 +41,7 @@ export default {
         editBook() {
             this.$el.querySelector(".inside_data_book").style.display = "none";
             this.$refs.bookEdit.$el.style.display = "flex";
-        }, 
+        },
         showDataBook() {
             this.$el.querySelector(".inside_data_book").style.display = "flex";
             this.bookStatus = this.book.status;
@@ -85,7 +86,15 @@ export default {
                 <option value="wanttoread">Хочу прочитать</option>
                 <option value="finishread">Закончил читать</option>
             </select>
-            <button @click="editBook" class="edit">Редактировать</button>
+            <svg :style="{ color: colorOpenButton }" class="edit" @click="editBook" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+            <g id="SVGRepo_iconCarrier">
+              <path d="M9 5H7C5.89543 5 5 5.89543 5 7V19C5 20.1046 5.89543 21 7 21H9M15 5H17C18.1046 5 19 5.89543 19 7V9" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+              <path d="M14.902 20.3343L12.7153 20.7716L13.1526 18.585C13.1914 18.3914 13.2865 18.2136 13.4261 18.074L17.5 14L19.5 12L21.4869 13.9869L19.4869 15.9869L15.413 20.0608C15.2734 20.2004 15.0956 20.2956 14.902 20.3343Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+              <path d="M9 5C9 3.89543 9.89543 3 11 3H13C14.1046 3 15 3.89543 15 5V7H9V5Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+            </g>
+          </svg>
         </div>
         <BookEditMenu @update="updateBook" @show="showDataBook" ref="bookEdit" :book="this.book"></BookEditMenu>
     </div>
